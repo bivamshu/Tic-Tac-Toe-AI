2e02deb2a5bf9d9f7d0bc7ea9cb8af351031988c
bivamshu <bivamshu.khadka86@gmail.com>
2025-05-03 11:08:19 +0545

    more sht

--- tictactoe.py
+++ tictactoe.py
@@ -12,12 +12,29 @@ screen.fill(BG_COLOR)
 class Board():
     def __init__(self):
         self.squares = np.zeros((ROWS, COLS))
+        self.empty_sqr = self.squares # list of squares
+        self.marked_squares = 0
     
     def mark_square(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_squares += 1
 
     def empty_sqr(self, row, col):
         return self.squares[row][col] == 0
+    
+    def isFull(self):
+        return self.marked_squares == 9
+    
+    def isEmpty(self, row, col):
+        return self.squares[row][col] == 0
+    
+    def get_empty_sqr(self):
+        empty_sqrs = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_sqr(row, col):
+                    empty_sqrs.append((row, col))
+        return empty_sqrs
 
 class Game:
     def __init__(self):
@@ -33,6 +50,26 @@ class Game:
         #horizontal 
         pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
+    
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            #draw cross
+            #desc line
+            start_desc = (col * SQSIZE + OFFSET, row *  SQSIZE + OFFSET)
+            end_desc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            #ascending line
+            start_asc = (col * SQSIZE + OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            end_asc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + OFFSET)
+
+            pygame.draw.line(screen, CROSS_COLOR, start_desc, end_desc, CROSS_WIDTH)
+            pygame.draw.line(screen, CROSS_COLOR, start_asc, end_asc, CROSS_WIDTH)
+
+        elif self.player == 2:
+            center = (col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2)
+            pygame.draw.circle(screen, CIRC_COLOR, center, RADIUS, CIRC_WIDTH)
+
+    def next_turn(self):
+        self.player = self.player % 2 + 1
 
 def main():
     #create game object
@@ -53,8 +90,9 @@ def main():
                 col = pos[0] // SQSIZE
                 
                 if board.empty_sqr(row, col):
-                    board.mark_square(row, col, 1) 
-                
+                    board.mark_square(row, col, game.player)
+                    game.draw_fig(row, col)
+                    game.next_turn()
 
         pygame.display.update()
 
